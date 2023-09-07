c80702bc70bd60ca7abe40d7230f262e5a06469d
Starovoytov Alexandr <43303621+stewkk@users.noreply.github.com>
2023-09-07 14:35:39 +0300

    Add testdata

--- source/serializer.cpp
+++ source/serializer.cpp
@@ -1,18 +1,19 @@
 #include "nnmcpp/serializer.hpp"
 
 #include <boost/algorithm/string/join.hpp>
+#include <fmt/format.h>
 
 namespace nnmcpp {
 
   std::string Serialize(const Info& info) {
     std::vector<std::string> fields = {
-        info.title.raw,
+	    boost::algorithm::join(std::vector<std::string>{info.title.ru_title, info.title.en_title}, " / "),
         info.translation.raw, /* TODO: strange encoding of translation type */
         info.title.year,
         boost::algorithm::join(
-            std::vector<std::string>{info.director.raw, info.country.raw, info.production.raw}, "/"),
+            std::vector<std::string>{info.director.raw, info.country.raw, info.production.raw}, " / "),
         "", /* TODO: place on hard drive */
-        info.video.raw,
+        fmt::format("{}x{}", info.video.width, info.video.height),
         info.quality.raw,
         info.duration.raw,
         info.genre.raw,
@@ -23,7 +24,7 @@ namespace nnmcpp {
         info.actor.raw,
         "" /* empty field */
     };
-    return boost::algorithm::join(fields, "\\");
+    return boost::algorithm::join(fields, " \\ ");
   }
 
 }  // namespace nnmcpp
